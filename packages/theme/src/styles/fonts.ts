be2f45942c647dcb23d7ddb8fb63c85ecdf9c69b
w3stside <doohhello@gmail.com>
2023-01-23 20:34:54 +0000

    pkg theme: update fonts

--- packages/theme/src/styles/fonts.ts
+++ packages/theme/src/styles/fonts.ts
@@ -1,91 +1,77 @@
+import { FONTS } from '@past3lle/assets'
 import { createGlobalStyle } from 'styled-components'
 
-import Roboto100 from '../../assets/fonts/Roboto/Roboto100.ttf'
-import Roboto100Italic from '../../assets/fonts/Roboto/Roboto100Italic.ttf'
-import Roboto900 from '../../assets/fonts/Roboto/Roboto-Black.ttf'
-import Roboto900Italic from '../../assets/fonts/Roboto/Roboto-BlackItalic.ttf'
-import Roboto700 from '../../assets/fonts/Roboto/Roboto-Bold.ttf'
-import Roboto700Italic from '../../assets/fonts/Roboto/Roboto-BoldItalic.ttf'
-import Roboto400Italic from '../../assets/fonts/Roboto/Roboto-Italic.ttf'
-import Roboto300 from '../../assets/fonts/Roboto/Roboto-Light.ttf'
-import Roboto300Italic from '../../assets/fonts/Roboto/Roboto-LightItalic.ttf'
-import Roboto500 from '../../assets/fonts/Roboto/Roboto-Medium.ttf'
-import Roboto500Italic from '../../assets/fonts/Roboto/Roboto-MediumItalic.ttf'
-import Roboto400 from '../../assets/fonts/Roboto/Roboto-Regular.ttf'
-
-const FontStyles = createGlobalStyle`
+export const FontStyles = createGlobalStyle`
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto100}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Thin}) format('truetype');
         font-style: normal;
         font-weight: 100;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto100Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_ThinItalic}) format('truetype');
         font-style: italic;
         font-weight: 100;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto300}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Light}) format('truetype');
         font-style: normal;
         font-weight: 300;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto300Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_LightItalic}) format('truetype');
         font-style: italic;
         font-weight: 300;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto400}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Regular}) format('truetype');
         font-style: normal;
         font-weight: 400;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto400Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Italic}) format('truetype');
         font-style: italic;
         font-weight: 400;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto500}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Medium}) format('truetype');
         font-style: normal;
         font-weight: 500;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto500Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_MediumItalic}) format('truetype');
         font-style: italic;
         font-weight: 500;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto700}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Bold}) format('truetype');
         font-style: normal;
         font-weight: 700;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto700Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_BoldItalic}) format('truetype');
         font-style: italic;
         font-weight: 700;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto900}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_Black}) format('truetype');
         font-style: normal;
         font-weight: 900;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${Roboto900Italic}) format('truetype');
+        src: url(${FONTS.FONT_Roboto_BlackItalic}) format('truetype');
         font-style: italic;
         font-weight: 900;
     }
 `
-
-export default FontStyles
