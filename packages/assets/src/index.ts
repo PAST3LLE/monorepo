52d43a1c74b45bbfbd3238f3057d8eadd39c2b5d
w3stside <doohhello@gmail.com>
2023-01-23 18:06:14 +0000

    pkg assets: changes
    - added assets
    - updated declarations

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,6 +1,64 @@
-// SVG
-import LogoCircle from './svg/pastelle-logo-circle.svg'
-import LogoFull from './svg/pastelle-logo-full.svg'
-import LogoShort from './svg/pastelle-logo-short.svg'
+// FONTS
+import FONT_Roboto_Black from './fonts/Roboto/Roboto-Black.ttf'
+import FONT_Roboto_BlackItalic from './fonts/Roboto/Roboto-BlackItalic.ttf'
+import FONT_Roboto_Bold from './fonts/Roboto/Roboto-Bold.ttf'
+import FONT_Roboto_BoldItalic from './fonts/Roboto/Roboto-BoldItalic.ttf'
+import FONT_Roboto_Italic from './fonts/Roboto/Roboto-Italic.ttf'
+import FONT_Roboto_Light from './fonts/Roboto/Roboto-Light.ttf'
+import FONT_Roboto_LightItalic from './fonts/Roboto/Roboto-LightItalic.ttf'
+import FONT_Roboto_Medium from './fonts/Roboto/Roboto-Medium.ttf'
+import FONT_Roboto_MediumItalic from './fonts/Roboto/Roboto-MediumItalic.ttf'
+import FONT_Roboto_Regular from './fonts/Roboto/Roboto-Regular.ttf'
+import FONT_Roboto_Thin from './fonts/Roboto/Roboto-Thin.ttf'
+import FONT_Roboto_ThinItalic from './fonts/Roboto/Roboto-ThinItalic.ttf'
+// PNG circle logo
+import PNG_LogoCircle from './png/pastelle-logo-circle.png'
+import PNG_LogoCircle_2x from './png/pastelle-logo-circle@2x.png'
+import PNG_LogoCircle_3x from './png/pastelle-logo-circle@3x.png'
+// PNG full logo
+import PNG_LogoFull from './png/pastelle-logo-full.png'
+import PNG_LogoFull_2x from './png/pastelle-logo-full@2x.png'
+import PNG_LogoFull_3x from './png/pastelle-logo-full@3x.png'
+// PNG short logo
+import PNG_LogoShort from './png/pastelle-logo-short.png'
+import PNG_LogoShort_2x from './png/pastelle-logo-short@2x.png'
+import PNG_LogoShort_3x from './png/pastelle-logo-short@3x.png'
+// SVGs
+import SVG_LogoCircle from './svg/pastelle-logo-circle.svg'
+import SVG_LogoFull from './svg/pastelle-logo-full.svg'
+import SVG_LogoShort from './svg/pastelle-logo-short.svg'
 
-export { LogoFull, LogoShort, LogoCircle }
+const PNG = {
+  PNG_LogoCircle
+}
+const SVG = {
+  SVG_LogoCircle,
+  SVG_LogoFull,
+  SVG_LogoShort,
+  PNG_LogoCircle_2x,
+  PNG_LogoCircle_3x,
+  // PNG full logo
+  PNG_LogoFull,
+  PNG_LogoFull_2x,
+  PNG_LogoFull_3x,
+  // PNG short logo
+  PNG_LogoShort,
+  PNG_LogoShort_2x,
+  PNG_LogoShort_3x
+}
+const FONTS = {
+  FONT_Roboto_Black,
+  FONT_Roboto_BlackItalic,
+  FONT_Roboto_Bold,
+  FONT_Roboto_BoldItalic,
+  FONT_Roboto_Italic,
+  FONT_Roboto_Light,
+  FONT_Roboto_LightItalic,
+  FONT_Roboto_Medium,
+  FONT_Roboto_MediumItalic,
+  FONT_Roboto_Regular,
+  FONT_Roboto_Thin,
+  FONT_Roboto_ThinItalic
+}
+
+export { SVG, PNG, FONTS }
