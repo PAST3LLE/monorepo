f8ce370dd2114794f4146b5f1ddfc47933be159d
w3stside <doohhello@gmail.com>
2023-01-23 23:14:47 +0000

    pkg assets: edit asset names

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,64 +1,64 @@
 // FONTS
-import FONT_Roboto_Black from './fonts/Roboto/Roboto-Black.ttf'
-import FONT_Roboto_BlackItalic from './fonts/Roboto/Roboto-BlackItalic.ttf'
-import FONT_Roboto_Bold from './fonts/Roboto/Roboto-Bold.ttf'
-import FONT_Roboto_BoldItalic from './fonts/Roboto/Roboto-BoldItalic.ttf'
-import FONT_Roboto_Italic from './fonts/Roboto/Roboto-Italic.ttf'
-import FONT_Roboto_Light from './fonts/Roboto/Roboto-Light.ttf'
-import FONT_Roboto_LightItalic from './fonts/Roboto/Roboto-LightItalic.ttf'
-import FONT_Roboto_Medium from './fonts/Roboto/Roboto-Medium.ttf'
-import FONT_Roboto_MediumItalic from './fonts/Roboto/Roboto-MediumItalic.ttf'
-import FONT_Roboto_Regular from './fonts/Roboto/Roboto-Regular.ttf'
-import FONT_Roboto_Thin from './fonts/Roboto/Roboto-Thin.ttf'
-import FONT_Roboto_ThinItalic from './fonts/Roboto/Roboto-ThinItalic.ttf'
+import Roboto_Black from './fonts/Roboto/Roboto-Black.ttf'
+import Roboto_BlackItalic from './fonts/Roboto/Roboto-BlackItalic.ttf'
+import Roboto_Bold from './fonts/Roboto/Roboto-Bold.ttf'
+import Roboto_BoldItalic from './fonts/Roboto/Roboto-BoldItalic.ttf'
+import Roboto_Italic from './fonts/Roboto/Roboto-Italic.ttf'
+import Roboto_Light from './fonts/Roboto/Roboto-Light.ttf'
+import Roboto_LightItalic from './fonts/Roboto/Roboto-LightItalic.ttf'
+import Roboto_Medium from './fonts/Roboto/Roboto-Medium.ttf'
+import Roboto_MediumItalic from './fonts/Roboto/Roboto-MediumItalic.ttf'
+import Roboto_Regular from './fonts/Roboto/Roboto-Regular.ttf'
+import Roboto_Thin from './fonts/Roboto/Roboto-Thin.ttf'
+import Roboto_ThinItalic from './fonts/Roboto/Roboto-ThinItalic.ttf'
 // PNG circle logo
-import PNG_LogoCircle from './png/pastelle-logo-circle.png'
-import PNG_LogoCircle_2x from './png/pastelle-logo-circle@2x.png'
-import PNG_LogoCircle_3x from './png/pastelle-logo-circle@3x.png'
+import LogoCircle_1x from './png/pastelle-logo-circle.png'
+import LogoCircle_2x from './png/pastelle-logo-circle@2x.png'
+import LogoCircle_3x from './png/pastelle-logo-circle@3x.png'
 // PNG full logo
-import PNG_LogoFull from './png/pastelle-logo-full.png'
-import PNG_LogoFull_2x from './png/pastelle-logo-full@2x.png'
-import PNG_LogoFull_3x from './png/pastelle-logo-full@3x.png'
+import LogoFull_1x from './png/pastelle-logo-full.png'
+import LogoFull_2x from './png/pastelle-logo-full@2x.png'
+import LogoFull_3x from './png/pastelle-logo-full@3x.png'
 // PNG short logo
-import PNG_LogoShort from './png/pastelle-logo-short.png'
-import PNG_LogoShort_2x from './png/pastelle-logo-short@2x.png'
-import PNG_LogoShort_3x from './png/pastelle-logo-short@3x.png'
+import LogoShort_1x from './png/pastelle-logo-short.png'
+import LogoShort_2x from './png/pastelle-logo-short@2x.png'
+import LogoShort_3x from './png/pastelle-logo-short@3x.png'
 // SVGs
-import SVG_LogoCircle from './svg/pastelle-logo-circle.svg'
-import SVG_LogoFull from './svg/pastelle-logo-full.svg'
-import SVG_LogoShort from './svg/pastelle-logo-short.svg'
+import LogoCircle from './svg/pastelle-logo-circle.svg'
+import LogoFull from './svg/pastelle-logo-full.svg'
+import LogoShort from './svg/pastelle-logo-short.svg'
 
 const PNG = {
-  PNG_LogoCircle,
-  PNG_LogoCircle_2x,
-  PNG_LogoCircle_3x,
+  LogoCircle_1x,
+  LogoCircle_2x,
+  LogoCircle_3x,
   // PNG full logo
-  PNG_LogoFull,
-  PNG_LogoFull_2x,
-  PNG_LogoFull_3x,
+  LogoFull_1x,
+  LogoFull_2x,
+  LogoFull_3x,
   // PNG short logo
-  PNG_LogoShort,
-  PNG_LogoShort_2x,
-  PNG_LogoShort_3x
+  LogoShort_1x,
+  LogoShort_2x,
+  LogoShort_3x
 }
 const SVG = {
-  SVG_LogoCircle,
-  SVG_LogoFull,
-  SVG_LogoShort
+  LogoCircle,
+  LogoFull,
+  LogoShort
 }
 const FONTS = {
-  FONT_Roboto_Black,
-  FONT_Roboto_BlackItalic,
-  FONT_Roboto_Bold,
-  FONT_Roboto_BoldItalic,
-  FONT_Roboto_Italic,
-  FONT_Roboto_Light,
-  FONT_Roboto_LightItalic,
-  FONT_Roboto_Medium,
-  FONT_Roboto_MediumItalic,
-  FONT_Roboto_Regular,
-  FONT_Roboto_Thin,
-  FONT_Roboto_ThinItalic
+  Roboto_Black,
+  Roboto_BlackItalic,
+  Roboto_Bold,
+  Roboto_BoldItalic,
+  Roboto_Italic,
+  Roboto_Light,
+  Roboto_LightItalic,
+  Roboto_Medium,
+  Roboto_MediumItalic,
+  Roboto_Regular,
+  Roboto_Thin,
+  Roboto_ThinItalic
 }
 
 export { SVG, PNG, FONTS }
