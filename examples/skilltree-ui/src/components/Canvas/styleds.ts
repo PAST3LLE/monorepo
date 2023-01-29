6ac36affac3e6cc4214a0c4fd11156d392952ae4
w3stside <doohhello@gmail.com>
2023-01-29 15:37:42 +0000

    examples: remove skilltree

--- examples/skilltree-ui/src/components/Canvas/styleds.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledCanvas = styled.canvas`
-  //   z-index: -1;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  opacity: 0.4;
-`
--- examples/skilltree-ui/src/theme/theme.d.ts
+++
@@ -1,7 +0,0 @@
-import { CustomTheme } from "@past3lle/theme"
-import { CustomTheme as LocalCustomTheme } from "./customTheme"
-
-declare module 'styled-components' {
-    // @ts-ignore - recursive error
-    export interface DefaultTheme extends LocalCustomTheme, CustomTheme {}
-}
