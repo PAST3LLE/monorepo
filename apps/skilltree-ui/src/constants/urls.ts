b50736ca05eedcea48f1e8b1d0c167d9fdc79212
w3stside <doohhello@gmail.com>
2023-01-29 16:06:09 +0000

    apps: skilltree-ui
    1. update html properties
    2. url config
    3. exports

---
+++ apps/skilltree-ui/src/constants/urls.ts
@@ -0,0 +1,1 @@
+export const SHOP_URL = 'https://pastelle.shop'
--- apps/skilltree-ui/src/views/SkilltreeView/index.tsx
+++ apps/skilltree-ui/src/views/SkilltreeView/index.tsx
@@ -1,16 +1,26 @@
 import { SkillsCanvas } from '../../components/Skills/SkillsCanvas'
 import { BlackBoldItalic, BlackHeader } from '../../components/Text'
-import { ArticleFadeIn, AutoRow, Header as HeaderPstl, Row } from '@past3lle/components'
+import { ArticleFadeIn, AutoRow, ExternalLink, Header as HeaderPstl, Pastellecon, Row } from '@past3lle/components'
+import { SHOP_URL } from 'constants/index'
 import React from 'react'
 import styled from 'styled-components/macro'
 
+const Skilltreecon = styled(Pastellecon)`
+  margin-bottom: -20px;
+  z-index: 1;
+  position: relative;
+  height: 50px;
+`
+
 const Header = () => (
   <HeaderPstl>
-    <Row gap="1rem">
-      <BlackHeader>PASTELLE SKILLTREE</BlackHeader>
+    <Row gap="1rem" height="100%">
+      <BlackHeader>
+        <Skilltreecon /> SKILLS
+      </BlackHeader>
       <AutoRow display={'inline-flex'} backgroundColor={'ghostwhite'} padding={'1rem'}>
         <BlackBoldItalic fontSize={'1.5rem'}>
-          VIEW CURRENT SKILLS AND ANY UPGRADABLES. HOVER OVER SKILL FOR MORE INFO.
+          ACQUIRE NEW SKILL IN <ExternalLink href={SHOP_URL}>THE FORGE</ExternalLink> AND UPGRADE THEM HERE
         </BlackBoldItalic>
       </AutoRow>
     </Row>
