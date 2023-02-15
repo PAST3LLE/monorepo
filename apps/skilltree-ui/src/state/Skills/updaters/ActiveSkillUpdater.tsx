d08aff7fd863ee3b9c05ba8a685e623bc936af8b
w3stside <doohhello@gmail.com>
2023-02-15 19:24:48 +0000

    apps: skilltree-ui - don't animate on mobile size

--- apps/skilltree-ui/src/state/Skills/updaters/ActiveSkillUpdater.tsx
+++ apps/skilltree-ui/src/state/Skills/updaters/ActiveSkillUpdater.tsx
@@ -1,24 +1,28 @@
 import { useSkillsAtom } from '..'
+import { MEDIA_WIDTHS } from '@past3lle/theme'
 import { toggleSelectedSkill } from 'components/Canvas/api/hooks'
 import { useEffect } from 'react'
 import { useSidePanelAtom } from 'state/SidePanel'
+import { useGetWindowSize } from 'state/WindowSize'
 
 export function ActiveSkillUpdater() {
   const [state] = useSkillsAtom()
   const [, openSidePanel] = useSidePanelAtom()
+  const [{ width = 0 }] = useGetWindowSize()
 
   useEffect(() => {
     const activeSkillNode = state.active ? document.getElementById(state.active) : null
 
     if (activeSkillNode) {
       openSidePanel('ACTIVE SKILL')
-      toggleSelectedSkill(state)
+      // only show lightning canvas on non-mobile
+      width > MEDIA_WIDTHS.upToSmall && toggleSelectedSkill(state)
       activeSkillNode.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' })
     } else {
       toggleSelectedSkill(undefined)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [state.active])
+  }, [state.active, width])
 
   return null
 }
