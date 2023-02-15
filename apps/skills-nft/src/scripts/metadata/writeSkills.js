5da38fefe4fd4c34c1004385c0476c5d175b397d
w3stside <doohhello@gmail.com>
2023-02-15 12:12:18 +0000

    apps: skill-nft writeSkills & nft metadata update

--- apps/skills-nft/src/scripts/metadata/writeSkills.js
+++ apps/skills-nft/src/scripts/metadata/writeSkills.js
@@ -1,7 +1,11 @@
 import fs from 'fs'
+import { createRequire } from 'node:module'
 
-import metadataToWrite from '../../skills/metadata.js'
 import { extractArgs } from '../../utils.js'
+
+const require = createRequire(import.meta.url)
+
+const metadataToWrite = require('../../skills/metadata.json')
 
 // ============================
 // ========== Flags ===========
@@ -17,34 +21,42 @@ async function writeSkills() {
 
   console.log('COLLECTION ID', collectionId)
   console.log('OVERWRITE?', overwrite)
-  console.log('ALL ARGS', args)
 
-  if (!collectionId || Number(collectionId) === undefined || isNaN(collectionId))
+  if (collectionId && (Number(collectionId) === undefined || isNaN(collectionId)))
     throw new Error(
-      'writeSkills.js script requires a collection id! Check your passed node args. e.g yarn writeSkills <collection_id>'
+      'writeSkills.js script requires a type NUMBER collection id! Check your passed node args. e.g yarn writeSkills <collection_id>'
     )
 
   try {
-    metadataToWrite.forEach(async (collection, i) => {
-      const folderIndex = (i + 1).toString()
-      const folderPath = 'apps/skills-nft/src/skills/' + folderIndex + '/metadata'
+    metadataToWrite.forEach(async (collection, collectionIdx) => {
+      const folderIndex = (collectionIdx + 1).toString()
+      const folderPath = './src/skills/' + folderIndex + '/metadata'
       const folderExists = fs.existsSync(folderPath)
 
-      if (!folderExists) {
-        console.log('Folder at path', folderPath, " doesn't exist yet, making...")
+      if (!folderExists || overwrite) {
+        console.log(
+          !folderExists
+            ? folderPath + " doesn't exist yet, making..."
+            : overwrite
+            ? 'Overwrite flag passed - overwriting folder in place.'
+            : ''
+        )
         // folder name relative to root (monorepo)
-        await fs.promises.mkdir(folderPath).catch((error) => {
+        await fs.promises.mkdir(folderPath, { recursive: true }).catch((error) => {
           console.error(error)
         })
       } else {
         console.log(`Folder ${folderIndex} already exists, skipping creation...`)
       }
-      return collection.map((metadata, index) =>
-        fs.promises.writeFile(
-          `${folderPath}/${((index + 1) * 1000).toString()}.json`,
-          JSON.stringify(metadata, null, ' ')
-        )
-      )
+      return collection.map((metadata) => {
+        // checks properties are there, errors and bails if not
+        _checkSkillMetadataSchema(metadata)
+        const id = metadata.properties.id.split('-')[1]
+        console.log('SKILL ID', id)
+        // e.g 1000 => 0000000000000000000000000000000000000000000000000000000000001000
+        const fileName = id.toString().padStart(64, 0)
+        return fs.promises.writeFile(`${folderPath}/${fileName}.json`, JSON.stringify(metadata, null, ' '))
+      })
     })
 
     console.log('Succesfully wrote new skills JSON metdata')
@@ -57,3 +69,49 @@ writeSkills().catch((error) => {
   console.error(error)
   process.exitCode = 1
 })
+
+function _checkSkillMetadataSchema(rawMetadata) {
+  if (
+    // props exist
+    !!rawMetadata.description &&
+    !!rawMetadata.image &&
+    !!rawMetadata.name &&
+    !!rawMetadata.properties &&
+    !!rawMetadata.properties.shopifyId &&
+    !!rawMetadata.properties.id &&
+    _checkSkillMetadataRarityType(rawMetadata.properties.rarity) &&
+    Array.isArray(rawMetadata.properties.dependencies) &&
+    !!rawMetadata.attributes &&
+    !!rawMetadata.attributes.theme &&
+    !!rawMetadata.attributes.theme.bg &&
+    !!rawMetadata.attributes.theme.altBg &&
+    !!rawMetadata.attributes.theme.color &&
+    !!rawMetadata.attributes.tags &&
+    Array.isArray(rawMetadata.attributes.tags) &&
+    // props are right type
+    typeof rawMetadata.description === 'string' &&
+    typeof rawMetadata.image === 'string' &&
+    typeof rawMetadata.name === 'string' &&
+    typeof rawMetadata.properties === 'object' &&
+    typeof rawMetadata.properties.rarity === 'string' &&
+    typeof rawMetadata.properties.shopifyId === 'string' &&
+    typeof rawMetadata.properties.id === 'string' &&
+    typeof rawMetadata.attributes === 'object' &&
+    typeof rawMetadata.attributes.theme === 'object' &&
+    typeof rawMetadata.attributes.theme.bg === 'string' &&
+    typeof rawMetadata.attributes.theme.altBg === 'string' &&
+    typeof rawMetadata.attributes.theme.color === 'string'
+  ) {
+    return true
+  } else {
+    throw new Error('INVALID SKILLS METADATA SCHEMA!')
+  }
+}
+
+function _checkSkillMetadataRarityType(rarity) {
+  if (rarity === 'common' || rarity === 'rare' || rarity === 'legendary' || rarity === 'epic') {
+    return true
+  } else {
+    throw new Error('INVALID SKILLS RARITY METADATA PROPERTY!')
+  }
+}
