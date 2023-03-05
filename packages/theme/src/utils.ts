aa30ce7fced2d5bee860fc1d5b4ccb33124369f8
w3stside <doohhello@gmail.com>
2023-03-05 18:14:54 +0000

    packages: theme - export generic url maker

--- packages/theme/src/utils.ts
+++ packages/theme/src/utils.ts
@@ -260,3 +260,14 @@ function _getPresetOptions(
     }
   }
 }
+
+export function urlToSimpleGenericImageSrcSet(url: string): GenericImageSrcSet<MediaWidths> {
+  return {
+    defaultUrl: url,
+    500: { '1x': url },
+    720: { '1x': url },
+    960: { '1x': url },
+    1280: { '1x': url },
+    1440: { '1x': url }
+  }
+}
