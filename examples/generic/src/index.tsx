5b584603ee337000fc91aea84924999631e50dbd
w3stside <doohhello@gmail.com>
2023-03-06 23:28:25 +0000

    examples: generic - utils example

--- examples/generic/src/index.tsx
+++ examples/generic/src/index.tsx
@@ -4,6 +4,8 @@ import {
   StaticGlobalCssProvider,
   ThemeProvider,
   createCustomTheme,
+  getThemeColourByKeyCurried,
+  getThemeColoursCurried,
   urlToSimpleGenericImageSrcSet
 } from '@past3lle/theme'
 import * as React from 'react'
@@ -23,11 +25,25 @@ const StaticCssProviders = () => (
 
 const appTheme = createCustomTheme({
   modes: {
-    DEFAULT: {},
-    DARK: {},
+    DEFAULT: {
+      red1: 'red',
+      red2: 'orange'
+    },
+    DARK: {
+      red1: 'indianred',
+      red2: 'darkred'
+    },
     LIGHT: {}
   }
 })
+
+/**
+ * Curried theme functions to get theme items
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const red1 = getThemeColourByKeyCurried(appTheme)('DARK', 'red1', 'red')
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const themeDark = getThemeColoursCurried(appTheme)('DARK')
 
 const container = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(container)
