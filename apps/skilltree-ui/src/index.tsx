44cbc5b330dc8d50916a492918cfdfd8930adaad
w3stside <doohhello@gmail.com>
2023-03-07 18:43:20 +0000

    apps: skilltree-ui - add SkilltreeBoard widget

--- apps/skilltree-ui/src/index.tsx
+++ apps/skilltree-ui/src/index.tsx
@@ -1,62 +1,35 @@
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import { FontCssProvider, StaticGlobalCssProvider, ThemeProvider, ThemedGlobalCssProvider } from '@past3lle/theme'
-import { AtomsDevtools } from 'dev/devTools'
-import React, { StrictMode } from 'react'
+import { SkilltreeBoardConnected, SkilltreeBoardConnectedProps, SkilltreeHeader } from '@past3lle/skilltree-widget'
+import { createTemplateTheme } from '@past3lle/theme'
+import { ASSETS_MAP } from 'assets'
+import React from 'react'
 import 'react-app-polyfill/ie11'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from 'reportWebVitals'
-import { MetadataUpdater } from 'state/Metadata/updaters/MetadataUpdater'
-import { SidePanelUpdater } from 'state/SidePanel/updater'
-import { SkillsUpdaters } from 'state/Skills/updaters'
-import { UserBalancesUpdater } from 'state/User/updaters'
-import { WindowSizeUpdater } from 'state/WindowSize/updaters'
-import { CUSTOM_THEME } from 'theme/customTheme'
-import { GothicFontCssProvider } from 'theme/fonts'
-import { CustomStaticGlobalCss, CustomThemeGlobalCss } from 'theme/global'
-import { SkilltreeView } from 'views/SkilltreeView'
-import { WagmiProvider, Web3Modal } from 'web3/config'
-
-const StaticCssProviders = () => (
-  <>
-    <StaticGlobalCssProvider />
-    <CustomStaticGlobalCss />
-  </>
-)
-
-const ThemedCssProviders = () => (
-  <>
-    <ThemedGlobalCssProvider />
-    <CustomThemeGlobalCss />
-  </>
-)
 
 const container = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(container)
 
+const skilltreeTheme = createTemplateTheme('SKILLTREE', {
+  DEFAULT: {
+    assetsMap: ASSETS_MAP
+  }
+})
+
+const SKILLTREE_CONFIG: SkilltreeBoardConnectedProps = {
+  config: {
+    appName: 'PSTL SKILLTREE',
+    appTheme: skilltreeTheme,
+    provider: {
+      projectId: process.env.REACT_APP_WALLETCONNECT_KEY || 'BROKEN'
+    }
+  }
+}
+
 root.render(
-  <StrictMode>
-    <Web3Modal />
-    <WagmiProvider>
-      {/* @ts-ignore */}
-      <AtomsDevtools>
-        {/* UPDATERS */}
-        <MetadataUpdater />
-        <UserBalancesUpdater />
-        <SkillsUpdaters />
-        <WindowSizeUpdater />
-        {/* THEME PROVIDERS */}
-        <GothicFontCssProvider />
-        <FontCssProvider />
-        <StaticCssProviders />
-        {/* @ts-ignore */}
-        <ThemeProvider themeExtension={CUSTOM_THEME}>
-          <ThemedCssProviders />
-          <SidePanelUpdater />
-          <SkilltreeView />
-        </ThemeProvider>
-      </AtomsDevtools>
-    </WagmiProvider>
-  </StrictMode>
+  <SkilltreeBoardConnected config={SKILLTREE_CONFIG.config}>
+    <SkilltreeHeader />
+  </SkilltreeBoardConnected>
 )
 
 // SERVICE WORKER (e.g USER OFFLINE USE)
