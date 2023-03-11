f001bc3157b072e052e15571a4740c1734b51527
w3stside <david.sato64@gmail.com>
2023-03-11 00:10:44 +0000

    packages: skillforge-widget: vectorsmap logic memo

--- packages/skillforge-widget/src/components/Canvas/index.tsx
+++ packages/skillforge-widget/src/components/Canvas/index.tsx
@@ -7,7 +7,7 @@ import {
   useSkillForgeMetadataMapReadAtom
 } from '@past3lle/skillforge-web3'
 import { convertToRomanNumerals } from '@past3lle/utils'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import { EMPTY_SKILL_IMAGE_HASH_LIST, MINIMUM_COLLECTION_BOARD_SIZE } from '../../constants/skills'
 import { useSkillsAtom } from '../../state/Skills'
@@ -23,15 +23,40 @@ export function SkillsCanvas() {
   const [metadataMap] = useSkillForgeMetadataMapReadAtom()
   const [{ balances }] = useSkillForgeBalancesAtom()
 
+  const VectorsMap = useMemo(
+    () =>
+      vectors.map(({ skillId, vector }) => {
+        if (!vector) return
+        const skillBalance = skillId && balances[skillId]
+        const missingSkill = !skillBalance || !skillId
+        const zeroBalance = !!skillBalance && BigNumber.from(skillBalance).isZero()
+
+        const props = !missingSkill
+          ? {
+              key: skillId,
+              metadata: metadataMap[skillId],
+              hasSkill: !zeroBalance
+            }
+          : { key: `EMPTY-${vector.X1}-${vector.Y1}`, metadata: EMPTY_METADATA, hasSkill: true }
+        return <Skillpoint {...props} vector={vector} />
+      }),
+    [balances, metadataMap, vectors]
+  )
+
   return (
     <SkillCanvasContainer style={{ position: 'relative' }}>
-      <Row width={'100%'} height={'12%'} justifyContent="space-between" style={{ position: 'relative' }}>
+      <Row
+        width={'100%'}
+        height={80}
+        /* minHeight="8rem" */ justifyContent="space-between"
+        style={{ position: 'relative' }}
+      >
         <SkillContainerAbsolute>
           {vectors.slice(0, MINIMUM_COLLECTION_BOARD_SIZE).map(({ vector }, idx) => {
             const idxToRoman = convertToRomanNumerals(idx + 1)
             if (!vector) return null
             return (
-              <SkillpointHeader key={idx} vector={new Vector(vector.X, vector.Y, vector.X1, 0)}>
+              <SkillpointHeader key={idx} vector={new Vector(vector.X, vector.Y, vector.X1, 0)} height={70}>
                 {idxToRoman}
               </SkillpointHeader>
             )
@@ -39,23 +64,7 @@ export function SkillsCanvas() {
         </SkillContainerAbsolute>
       </Row>
       <SkillInnerCanvasContainer height={'100%'} width="100%" style={{ position: 'relative' }} id="CANVAS-CONTAINER">
-        <SkillContainerAbsolute>
-          {vectors.map(({ skillId, vector }) => {
-            if (!vector) return
-            const skillBalance = skillId && balances[skillId]
-            const missingSkill = !skillBalance || !skillId
-            const zeroBalance = !!skillBalance && BigNumber.from(skillBalance).isZero()
-
-            const props = !missingSkill
-              ? {
-                  key: skillId,
-                  metadata: metadataMap[skillId],
-                  hasSkill: !zeroBalance
-                }
-              : { key: `EMPTY-${vector.X1}-${vector.Y1}`, metadata: EMPTY_METADATA, hasSkill: true }
-            return <Skillpoint {...props} vector={vector} />
-          })}
-        </SkillContainerAbsolute>
+        <SkillContainerAbsolute>{VectorsMap}</SkillContainerAbsolute>
         {/* CANVAS */}
         <LightningCanvas />
       </SkillInnerCanvasContainer>
