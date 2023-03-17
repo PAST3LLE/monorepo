574b5129c615de64dd6fc86a8040432b9188f4af
w3stside <david.sato64@gmail.com>
2023-03-17 18:47:41 +0000

    packages: carousel - export more styledds

--- packages/carousel/src/index.ts
+++ packages/carousel/src/index.ts
@@ -2,8 +2,10 @@ import AnimatedCarousel from './components/AnimatedCarousel'
 import ButtonCarousel from './components/ButtonCarousel'
 // common components
 import {
+  AnimatedDivContainer as StyledAnimatedDivContainer,
   CarouselContainer as StyledCarouselContainer,
-  CarouselItemContainer as StyledCarouselItemContainer
+  CarouselItemContainer as StyledCarouselItemContainer,
+  ScrollerContainer as StyledScrollerContainer
 } from './components/Common/styleds'
 import HorizontalSwipeCarousel from './components/HorizontalSwipeCarousel'
 import VerticalSwipeCarousel from './components/VerticalSwipeCarousel'
@@ -26,6 +28,8 @@ export {
   // Styleds
   StyledCarouselContainer,
   StyledCarouselItemContainer,
+  StyledAnimatedDivContainer,
+  StyledScrollerContainer,
   // Types
   BaseAnimatedCarouselProps,
   BaseCarouselProps,
