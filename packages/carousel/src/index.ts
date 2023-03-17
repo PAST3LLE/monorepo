66e4f7659a3c1f272c3b9de8bdbc9922b72888b3
w3stside <david.sato64@gmail.com>
2023-03-17 18:26:52 +0000

    packages: carousel - export more types

--- packages/carousel/src/index.ts
+++ packages/carousel/src/index.ts
@@ -2,7 +2,14 @@ import AnimatedCarousel from './components/AnimatedCarousel'
 import ButtonCarousel from './components/ButtonCarousel'
 import HorizontalSwipeCarousel from './components/HorizontalSwipeCarousel'
 import VerticalSwipeCarousel from './components/VerticalSwipeCarousel'
-import type { BaseAnimatedCarouselProps, BaseCarouselProps, ButtonCarouselProps, TouchAction } from './types'
+import type {
+  BaseAnimatedCarouselProps,
+  BaseCarouselProps,
+  ButtonCarouselProps,
+  CarouselChildrenProps,
+  TouchAction,
+  WithTouchAction
+} from './types'
 
 export {
   AnimatedCarousel,
@@ -12,5 +19,7 @@ export {
   BaseAnimatedCarouselProps,
   BaseCarouselProps,
   ButtonCarouselProps,
-  TouchAction
+  CarouselChildrenProps,
+  TouchAction,
+  WithTouchAction
 }
