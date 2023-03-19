462a49a5b52313fd8c2d947656e93019fdc684db
w3stside <david.sato64@gmail.com>
2023-03-19 10:11:22 +0000

    packages: hooks - fix window size

--- packages/hooks/src/useWindowSize.ts
+++ packages/hooks/src/useWindowSize.ts
@@ -1,23 +1,40 @@
+import { MEDIA_WIDTHS, MediaWidths } from '@past3lle/theme'
+import { isMobile } from '@past3lle/utils'
 import { useEffect, useState } from 'react'
 
+interface WindowSize {
+  width: number | undefined
+  height: number | undefined
+  ar: number | undefined
+}
+
+interface SpecificEvent<E extends EventTarget> extends Event {
+  target: E
+}
+
 const IS_CLIENT = window instanceof Window || typeof window === 'object'
-let handler: (() => void) | undefined = undefined
+let handler: ((e: SpecificEvent<Window>) => void) | undefined = undefined
 export function useWindowSize() {
-  const [windowSize, setWindowSize] = useState(_getSize)
+  const [windowSize, setWindowSize] = useState<WindowSize | null>({
+    width: undefined,
+    height: undefined,
+    ar: undefined
+  })
   useEffect(() => {
     handler =
       handler ||
-      function handleCheckWindowSize() {
-        setWindowSize(_getSize())
+      function handleCheckWindowSize(e: SpecificEvent<Window>) {
+        console.debug('EVENT', e)
+        setWindowSize(_getSize(e))
       }
 
     if (handler && IS_CLIENT) {
-      handler()
-      window.addEventListener('resize', handler)
+      handler({ target: { innerHeight: window.innerHeight, innerWidth: window.innerWidth } } as SpecificEvent<Window>)
+      window.addEventListener('resize', handler as EventListener)
     }
 
     return () => {
-      handler && window.removeEventListener('resize', handler)
+      handler && window.removeEventListener('resize', handler as EventListener)
       handler = undefined
     }
   }, [])
@@ -25,13 +42,31 @@ export function useWindowSize() {
   return windowSize
 }
 
-function _getSize() {
+function _getSize(e: SpecificEvent<Window>): WindowSize {
   return {
-    width: IS_CLIENT ? window.innerWidth : undefined,
-    height: IS_CLIENT ? window.innerHeight : undefined,
+    width: e.target?.innerWidth,
+    height: e.target?.innerHeight,
     get ar() {
       if (!IS_CLIENT || !this.width || !this.height) return undefined
       return this.width / this.height
     }
   }
+}
+
+export function useWindowSmallerThan(media: MediaWidths) {
+  const sizes = useWindowSize()
+
+  return Boolean(sizes?.width && sizes.width <= media)
+}
+
+export const useIsExtraSmallMediaWidth = () => useWindowSmallerThan(MEDIA_WIDTHS.upToExtraSmall)
+export const useIsSmallMediaWidth = () => useWindowSmallerThan(MEDIA_WIDTHS.upToSmall)
+export const useIsMediumMediaWidth = () => useWindowSmallerThan(MEDIA_WIDTHS.upToMedium)
+export const useIsLargeMediaWidth = () => useWindowSmallerThan(MEDIA_WIDTHS.upToLarge)
+export const useIsExtraLargeMediaWidth = () => useWindowSmallerThan(MEDIA_WIDTHS.upToExtraLarge)
+
+export function useIsMobile() {
+  const isMobileWidth = useIsSmallMediaWidth()
+
+  return isMobile || isMobileWidth
 }
