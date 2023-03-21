790bf2847727e3dfe620492345a45be679418da9
w3stside <david.sato64@gmail.com>
2023-03-21 14:43:40 +0000

    packages: hooks - update useOnResize

--- packages/hooks/src/useOnResize.ts
+++ packages/hooks/src/useOnResize.ts
@@ -2,9 +2,21 @@ import { useEffect } from 'react'
 
 import { useWindowSize } from './useWindowSize'
 
-export function useOnResize(callback: (...params: any[]) => any, ...deps: any[]) {
+/**
+ * @name useOnResize
+ * @description Detects window re-sizing and calls a passed in callback based on a condition, if required.
+ * @example
+  // close open nav on window resize
+  useOnResize(() => setIsNavOpen(false), isNavOpen)
+ * @param callback function to call on window resize. If condition param is given, only fires when true
+ * @param condition [optional] condition necessary to be true before firing callback. Defaults to <true>
+ */
+export function useOnResize(callback: (...params: any[]) => any, condition = true) {
   const windowSizes = useWindowSize()
   useEffect(() => {
-    callback()
-  }, [callback, windowSizes, ...deps])
+    if (condition) {
+      callback()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [windowSizes])
 }
