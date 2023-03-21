0d8c4c0c3a8deecd5d5e6e9039df3bec6466af13
w3stside <david.sato64@gmail.com>
2023-03-21 18:41:29 +0000

    packages: assets + theme - tree shaking

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,64 +1,61 @@
 // FONTS
-import Roboto_Black from './fonts/Roboto/Roboto-Black.ttf'
-import Roboto_BlackItalic from './fonts/Roboto/Roboto-BlackItalic.ttf'
-import Roboto_Bold from './fonts/Roboto/Roboto-Bold.ttf'
-import Roboto_BoldItalic from './fonts/Roboto/Roboto-BoldItalic.ttf'
-import Roboto_Italic from './fonts/Roboto/Roboto-Italic.ttf'
-import Roboto_Light from './fonts/Roboto/Roboto-Light.ttf'
-import Roboto_LightItalic from './fonts/Roboto/Roboto-LightItalic.ttf'
-import Roboto_Medium from './fonts/Roboto/Roboto-Medium.ttf'
-import Roboto_MediumItalic from './fonts/Roboto/Roboto-MediumItalic.ttf'
-import Roboto_Regular from './fonts/Roboto/Roboto-Regular.ttf'
-import Roboto_Thin from './fonts/Roboto/Roboto-Thin.ttf'
-import Roboto_ThinItalic from './fonts/Roboto/Roboto-ThinItalic.ttf'
+import FONT_Roboto_Black from './fonts/Roboto/Roboto-Black.ttf'
+import FONT_Roboto_BlackItalic from './fonts/Roboto/Roboto-BlackItalic.ttf'
+import FONT_Roboto_Bold from './fonts/Roboto/Roboto-Bold.ttf'
+import FONT_Roboto_BoldItalic from './fonts/Roboto/Roboto-BoldItalic.ttf'
+import FONT_Roboto_Italic from './fonts/Roboto/Roboto-Italic.ttf'
+import FONT_Roboto_Light from './fonts/Roboto/Roboto-Light.ttf'
+import FONT_Roboto_LightItalic from './fonts/Roboto/Roboto-LightItalic.ttf'
+import FONT_Roboto_Medium from './fonts/Roboto/Roboto-Medium.ttf'
+import FONT_Roboto_MediumItalic from './fonts/Roboto/Roboto-MediumItalic.ttf'
+import FONT_Roboto_Regular from './fonts/Roboto/Roboto-Regular.ttf'
+import FONT_Roboto_Thin from './fonts/Roboto/Roboto-Thin.ttf'
+import FONT_Roboto_ThinItalic from './fonts/Roboto/Roboto-ThinItalic.ttf'
 // PNG circle logo
-import LogoCircle_1x from './png/pastelle-logo-circle.png'
-import LogoCircle_2x from './png/pastelle-logo-circle@2x.png'
-import LogoCircle_3x from './png/pastelle-logo-circle@3x.png'
+import PNG_LogoCircle_1x from './png/pastelle-logo-circle.png'
+import PNG_LogoCircle_2x from './png/pastelle-logo-circle@2x.png'
+import PNG_LogoCircle_3x from './png/pastelle-logo-circle@3x.png'
 // PNG full logo
-import LogoFull_1x from './png/pastelle-logo-full.png'
-import LogoFull_2x from './png/pastelle-logo-full@2x.png'
-import LogoFull_3x from './png/pastelle-logo-full@3x.png'
+import PNG_LogoFull_1x from './png/pastelle-logo-full.png'
+import PNG_LogoFull_2x from './png/pastelle-logo-full@2x.png'
+import PNG_LogoFull_3x from './png/pastelle-logo-full@3x.png'
 // PNG short logo
-import LogoShort_1x from './png/pastelle-logo-short.png'
-import LogoShort_2x from './png/pastelle-logo-short@2x.png'
-import LogoShort_3x from './png/pastelle-logo-short@3x.png'
+import PNG_LogoShort_1x from './png/pastelle-logo-short.png'
+import PNG_LogoShort_2x from './png/pastelle-logo-short@2x.png'
+import PNG_LogoShort_3x from './png/pastelle-logo-short@3x.png'
 // SVGs
-import LogoCircle from './svg/pastelle-logo-circle.svg'
-import LogoFull from './svg/pastelle-logo-full.svg'
-import LogoShort from './svg/pastelle-logo-short.svg'
+import SVG_LogoCircle from './svg/pastelle-logo-circle.svg'
+import SVG_LogoFull from './svg/pastelle-logo-full.svg'
+import SVG_LogoShort from './svg/pastelle-logo-short.svg'
 
-const PNG = {
-  LogoCircle_1x,
-  LogoCircle_2x,
-  LogoCircle_3x,
+export {
+  // SVG
+  SVG_LogoCircle,
+  SVG_LogoFull,
+  SVG_LogoShort,
+  // PNG circle logo
+  PNG_LogoCircle_1x,
+  PNG_LogoCircle_2x,
+  PNG_LogoCircle_3x,
   // PNG full logo
-  LogoFull_1x,
-  LogoFull_2x,
-  LogoFull_3x,
+  PNG_LogoFull_1x,
+  PNG_LogoFull_2x,
+  PNG_LogoFull_3x,
   // PNG short logo
-  LogoShort_1x,
-  LogoShort_2x,
-  LogoShort_3x
+  PNG_LogoShort_1x,
+  PNG_LogoShort_2x,
+  PNG_LogoShort_3x,
+  // FONTS
+  FONT_Roboto_Black,
+  FONT_Roboto_BlackItalic,
+  FONT_Roboto_Bold,
+  FONT_Roboto_BoldItalic,
+  FONT_Roboto_Italic,
+  FONT_Roboto_Light,
+  FONT_Roboto_LightItalic,
+  FONT_Roboto_Medium,
+  FONT_Roboto_MediumItalic,
+  FONT_Roboto_Regular,
+  FONT_Roboto_Thin,
+  FONT_Roboto_ThinItalic
 }
-const SVG = {
-  LogoCircle,
-  LogoFull,
-  LogoShort
-}
-const FONTS = {
-  Roboto_Black,
-  Roboto_BlackItalic,
-  Roboto_Bold,
-  Roboto_BoldItalic,
-  Roboto_Italic,
-  Roboto_Light,
-  Roboto_LightItalic,
-  Roboto_Medium,
-  Roboto_MediumItalic,
-  Roboto_Regular,
-  Roboto_Thin,
-  Roboto_ThinItalic
-}
-
-export { SVG, PNG, FONTS }
--- packages/theme/src/styles/fonts.ts
+++ packages/theme/src/styles/fonts.ts
@@ -1,76 +1,89 @@
-import { FONTS } from '@past3lle/assets'
+import {
+  FONT_Roboto_Black,
+  FONT_Roboto_BlackItalic,
+  FONT_Roboto_Bold,
+  FONT_Roboto_BoldItalic,
+  FONT_Roboto_Italic,
+  FONT_Roboto_Light,
+  FONT_Roboto_LightItalic,
+  FONT_Roboto_Medium,
+  FONT_Roboto_MediumItalic,
+  FONT_Roboto_Regular,
+  FONT_Roboto_Thin,
+  FONT_Roboto_ThinItalic
+} from '@past3lle/assets'
 import { createGlobalStyle } from 'styled-components'
 
 export const FontCssProvider = createGlobalStyle`
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Thin}) format('truetype');
+        src: url(${FONT_Roboto_Thin}) format('truetype');
         font-style: normal;
         font-weight: 100;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_ThinItalic}) format('truetype');
+        src: url(${FONT_Roboto_ThinItalic}) format('truetype');
         font-style: italic;
         font-weight: 100;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Light}) format('truetype');
+        src: url(${FONT_Roboto_Light}) format('truetype');
         font-style: normal;
         font-weight: 300;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_LightItalic}) format('truetype');
+        src: url(${FONT_Roboto_LightItalic}) format('truetype');
         font-style: italic;
         font-weight: 300;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Regular}) format('truetype');
+        src: url(${FONT_Roboto_Regular}) format('truetype');
         font-style: normal;
         font-weight: 400;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Italic}) format('truetype');
+        src: url(${FONT_Roboto_Italic}) format('truetype');
         font-style: italic;
         font-weight: 400;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Medium}) format('truetype');
+        src: url(${FONT_Roboto_Medium}) format('truetype');
         font-style: normal;
         font-weight: 500;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_MediumItalic}) format('truetype');
+        src: url(${FONT_Roboto_MediumItalic}) format('truetype');
         font-style: italic;
         font-weight: 500;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Bold}) format('truetype');
+        src: url(${FONT_Roboto_Bold}) format('truetype');
         font-style: normal;
         font-weight: 700;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_BoldItalic}) format('truetype');
+        src: url(${FONT_Roboto_BoldItalic}) format('truetype');
         font-style: italic;
         font-weight: 700;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_Black}) format('truetype');
+        src: url(${FONT_Roboto_Black}) format('truetype');
         font-style: normal;
         font-weight: 900;
     }
     @font-face {
         font-family: 'Roboto';
-        src: url(${FONTS.Roboto_BlackItalic}) format('truetype');
+        src: url(${FONT_Roboto_BlackItalic}) format('truetype');
         font-style: italic;
         font-weight: 900;
     }
