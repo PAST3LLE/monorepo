62580e7b30d7b299449264b5a19e9f67b5600417
w3stside <david.sato64@gmail.com>
2023-03-29 00:01:22 +0100

    trim fat on skillforge-ui & landing-ui

--- apps/skillforge-ui/craco.config.js
+++ apps/skillforge-ui/craco.config.js
@@ -14,11 +14,11 @@ module.exports = {
     configure: config => {
       const fallback = config.resolve.fallback || {};
       Object.assign(fallback, {
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
         assert: require.resolve("assert"),
-        http: require.resolve("stream-http"),
+        crypto: require.resolve("crypto-browserify"),
         https: require.resolve("https-browserify"),
+        stream: require.resolve("stream-browserify"),
+        http: require.resolve("stream-http"),
         os: require.resolve("os-browserify"),
         url: require.resolve("url"),
       });
