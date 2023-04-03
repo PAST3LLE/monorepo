f81945d8428a4a9eaaf4be251f38769660f4e13d
w3stside <david.sato64@gmail.com>
2023-04-03 23:42:03 +0100

    skillforge-ui: remove now added walletImages prop

--- apps/skillforge-ui/src/views/index.tsx
+++ apps/skillforge-ui/src/views/index.tsx
@@ -51,10 +51,6 @@ const WEB3_PROPS: PstlWeb3ModalProps = {
     },
     w3m: {
       projectId: process.env.REACT_APP_WEB3MODAL_ID as string,
-      walletImages: {
-        web3auth: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
-        safe: 'https://user-images.githubusercontent.com/3975770/212338977-5968eae5-bb1b-4e71-8f82-af5282564c66.png'
-      },
       themeVariables: {
         '--w3m-background-color': skillforgeTheme.blackOpaque,
         '--w3m-accent-color': '#525291',
