afb4467d0cbf19b0556de2a6844887d0fad28df5
w3stside <david.sato64@gmail.com>
2023-04-03 23:41:46 +0100

    web3-modal: add default chains/wallets images

---
+++ packages/web3-modal/src/constants/chains.ts
@@ -0,0 +1,7 @@
+export const CHAIN_IMAGES = {
+  1: 'https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg',
+  5: 'https://icons.llamao.fi/icons/chains/rsz_ethereum.jpg',
+  10: 'https://icons.llamao.fi/icons/chains/rsz_optimism.jpg',
+  100: 'https://icons.llamao.fi/icons/chains/rsz_gnosis.jpg',
+  80001: 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg'
+}
---
+++ packages/web3-modal/src/constants/wallets.ts
@@ -0,0 +1,4 @@
+export const WALLET_IMAGES = {
+  web3auth: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
+  safe: 'https://user-images.githubusercontent.com/3975770/212338977-5968eae5-bb1b-4e71-8f82-af5282564c66.png'
+}
