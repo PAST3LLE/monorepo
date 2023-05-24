5610cd0d4c6db763d8c910483a90143ef6c5faa2
w3stside <david.sato64@gmail.com>
2023-05-24 17:49:23 +0100

    skillforge-ui: use zlib

--- apps/skillforge-ui/craco.config.js
+++ apps/skillforge-ui/craco.config.js
@@ -21,6 +21,7 @@ module.exports = {
         http: require.resolve("stream-http"),
         os: require.resolve("os-browserify"),
         url: require.resolve("url"),
+        zlib: require.resolve("browserify-zlib"),
       });
       config.resolve.fallback = fallback;
       config.plugins = (config.plugins || []).concat([
