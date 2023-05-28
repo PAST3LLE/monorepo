c30a7e248535a04e0c6ff8986d0880697896d1e3
w3stside <david.sato64@gmail.com>
2023-05-28 21:35:33 +0100

    skillforge-web3: useSupportedChainId uses user config state

--- packages/skillforge-web3/src/hooks/useSkillForgeSupportedChainId.ts
+++ packages/skillforge-web3/src/hooks/useSkillForgeSupportedChainId.ts
@@ -1,12 +1,20 @@
 import { devDebug } from '@past3lle/utils'
 import { usePstlConnection } from '@past3lle/web3-modal'
-import { goerli } from 'wagmi'
+import { useEffect } from 'react'
+import { goerli } from 'wagmi/chains'
+
+import { useSkillForgeGetSupportedChainsAtom } from '../state/UserConfig'
 
 export function useSupportedChain() {
+  const [supportedChains = []] = useSkillForgeGetSupportedChainsAtom()
   const [, , { chain: rawChain }] = usePstlConnection()
-  const chain = rawChain || goerli
 
-  !chain && devDebug('[useSupportedChainId]::Chain ID undefined. Defaulting to GOERLI [5]')
+  const currentChainSupported = rawChain && supportedChains.includes(rawChain)
+  const chain = currentChainSupported ? rawChain : goerli
+
+  useEffect(() => {
+    !currentChainSupported && devDebug('[useSupportedChainId]::Chain ID undefined. Defaulting to GOERLI [5]')
+  }, [rawChain])
 
   return chain
 }
