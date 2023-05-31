c6798ea42154be12d94c3cedc51afb1f997f4c35
w3stside <david.sato64@gmail.com>
2023-05-31 19:38:28 +0100

    skillforge-widget: fix for new mergeManager type

--- packages/skillforge-widget/src/fixtures/config.ts
+++ packages/skillforge-widget/src/fixtures/config.ts
@@ -30,12 +30,12 @@ const DEFAULT_CONFIG_PROPS: any = {
   contractAddresses: {
     [5]: {
       collectionsManager: '0x9e8e103ed51A18E92c0938573f9b7fB4A393083a',
-      unlockManager: '0x0B397B88C96E22E63D6D9b802df62fe40bB1B544'
+      mergeManager: '0x0B397B88C96E22E63D6D9b802df62fe40bB1B544'
     },
     // TODO: change to mumbai addresses
     [80001]: {
       collectionsManager: '0x9e8e103ed51A18E92c0938573f9b7fB4A393083a',
-      unlockManager: '0x0B397B88C96E22E63D6D9b802df62fe40bB1B544'
+      mergeManager: '0x0B397B88C96E22E63D6D9b802df62fe40bB1B544'
     }
   }
 }
