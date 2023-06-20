6a3fd8860071bb0d4c7fb226603faa7f14c3f4a9
w3stside <david.sato64@gmail.com>
2023-06-20 19:07:29 +0100

    skillforge-ui: update chains

--- apps/skillforge-ui/src/web3/config/chains.ts
+++ apps/skillforge-ui/src/web3/config/chains.ts
@@ -1,3 +1,3 @@
 import { goerli, polygon, polygonMumbai } from 'wagmi/chains'
 
-export const SUPPORTED_CHAINS = [goerli, polygon, polygonMumbai]
+export const SUPPORTED_CHAINS = process.env.NODE_ENV === 'production' ? [polygon] : [polygonMumbai, goerli]
