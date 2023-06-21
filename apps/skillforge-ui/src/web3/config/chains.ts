564b41d96c440135ef602fa5e6e775e1f8185e7c
w3stside <david.sato64@gmail.com>
2023-06-21 18:08:01 +0100

    skillforge-ui: local is single network goerli

--- apps/skillforge-ui/src/web3/config/chains.ts
+++ apps/skillforge-ui/src/web3/config/chains.ts
@@ -1,3 +1,3 @@
-import { goerli, polygon, polygonMumbai } from 'wagmi/chains'
+import { polygon, goerli } from 'wagmi/chains'
 
-export const SUPPORTED_CHAINS = process.env.NODE_ENV === 'production' ? [polygon] : [polygonMumbai, goerli]
+export const SUPPORTED_CHAINS = process.env.NODE_ENV === 'production' ? [polygon] : [goerli]
