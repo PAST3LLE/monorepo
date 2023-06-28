a76dcc98465b31dcff920a3f9be9a9252c7687ac
w3stside <david.sato64@gmail.com>
2023-06-28 23:11:40 +0100

    components: spinner invert prop

--- packages/components/src/Loaders/index.tsx
+++ packages/components/src/Loaders/index.tsx
@@ -3,12 +3,19 @@ import { rotateKeyframe } from '@past3lle/theme'
 import React from 'react'
 import styled from 'styled-components/macro'
 
-export const Spinner = styled.img<{ size?: number }>`
+export const Spinner = styled.img<{ size?: number; invertColor?: boolean }>`
   animation: 2s ${rotateKeyframe} linear infinite;
   width: ${({ size = 16 }) => size}px;
   height: ${({ size = 16 }) => size}px;
+
+  filter: inverse(${({ invertColor }) => (invertColor ? 1 : 0)});
 `
 
-export const SpinnerCircle = ({ size, src = SVG_LoadingCircleLight }: { src?: string; size?: number }) => (
-  <Spinner src={src} size={size} />
-)
+export const SpinnerCircle = ({
+  size,
+  src = SVG_LoadingCircleLight
+}: {
+  src?: string
+  size?: number
+  invertColor?: boolean
+}) => <Spinner src={src} size={size} />
