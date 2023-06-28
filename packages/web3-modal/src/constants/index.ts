9bac832287786ac4ac4b7771173f5864e200fb4c
w3stside <david.sato64@gmail.com>
2023-06-28 21:21:25 +0100

    web3-modal: base pstl modal z-index

--- packages/web3-modal/src/constants/index.ts
+++ packages/web3-modal/src/constants/index.ts
@@ -5,7 +5,7 @@ export const SOCIAL_LOGO = 'https://www.getopensocial.com/wp-content/uploads/202
 export const WALLETCONNECT_LOGO =
   'https://repository-images.githubusercontent.com/204001588/a5169900-c66c-11e9-8592-33c7334dfd6d'
 export enum Z_INDICES {
-  PSTL = 999,
+  PSTL = 900,
   W3M = 1000,
   W3A = 1000
 }
