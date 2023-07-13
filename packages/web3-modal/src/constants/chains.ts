041d8153b6959edda9bccb223fe229b6bac3f8ce
w3stside <david.sato64@gmail.com>
2023-07-13 13:24:47 +0100

    web3-modal: chains in cosmos and logos

--- packages/web3-modal/src/constants/chains.ts
+++ packages/web3-modal/src/constants/chains.ts
@@ -1,8 +1,8 @@
 export const CHAIN_IMAGES: Record<number, string> = {
-  1: 'https://cryptologos.cc/logos/versions/ethereum-eth-logo-colored.svg?v=025',
-  5: 'https://cryptologos.cc/logos/versions/ethereum-eth-logo-colored.svg?v=025',
-  10: 'https://icons.llamao.fi/icons/chains/rsz_optimism.jpg',
-  100: 'https://icons.llamao.fi/icons/chains/rsz_gnosis.jpg',
+  1: 'https://cryptologos.cc/logos/versions/ethereum-eth-logo-colored.svg',
+  5: 'https://cryptologos.cc/logos/versions/ethereum-eth-logo-colored.svg',
+  10: 'https://cryptologos.cc/logos/optimism-ethereum-op-logo.svg',
+  100: 'https://cryptologos.cc/logos/gnosis-gno-gno-logo.svg',
   137: 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg',
   80001: 'https://icons.llamao.fi/icons/chains/rsz_polygon.jpg'
 }
