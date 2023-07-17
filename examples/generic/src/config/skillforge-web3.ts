8fbb558070f956057f6a3e4549e72871a687c798
w3stside <david.sato64@gmail.com>
2023-07-17 11:40:18 +0200

    example: type

--- examples/generic/src/config/skillforge-web3.ts
+++ examples/generic/src/config/skillforge-web3.ts
@@ -12,18 +12,18 @@ import { goerli } from 'wagmi/chains'
 const WALLETCONNECT_TEST_ID = 'a01e2f3b7c64ff495f9cb28e4e2d4b49'
 // TESTING KEY DO NOT USE IN PROD
 const WEB3AUTH_TEST_ID = 'BHloyoLW113nGn-mIfeeNqj2U0wNCXa4y83xLnR6d3FELPMz_oZ7rbY4ZEO3r0MVjQ_LX92obu1ta0NknOwfvtU'
-const DEFAULT_PROPS: PstlWeb3ModalProps = {
+const DEFAULT_PROPS: PstlWeb3ModalProps<5> = {
   appName: 'COSMOS APP',
   chains: [goerli],
   modals: {
-    w3m: {
+    walletConnect: {
       projectId: WALLETCONNECT_TEST_ID,
       walletImages: {
         web3auth: 'https://web3auth.io/images/w3a-L-Favicon-1.svg',
         safe: 'https://user-images.githubusercontent.com/3975770/212338977-5968eae5-bb1b-4e71-8f82-af5282564c66.png'
       }
     },
-    w3a: {
+    web3auth: {
       appName: 'SKILLFORGE TEST',
       projectId: WEB3AUTH_TEST_ID,
       network: 'mainnet'
