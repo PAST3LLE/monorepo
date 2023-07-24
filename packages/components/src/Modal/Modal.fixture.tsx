9437895367b208935595dab2f91fa119afe0807c
w3stside <david.sato64@gmail.com>
2023-07-24 14:29:28 +0100

    components: replace @reach/dialog pkg w/ minimum dialog code

--- packages/components/src/Modal/Modal.fixture.tsx
+++ packages/components/src/Modal/Modal.fixture.tsx
@@ -9,8 +9,8 @@ export default {
     </Modal>
   ),
   smallModal: (
-    <Modal isOpen onDismiss={console.debug} styleProps={{ mainBackgroundColor: 'red' }} maxHeight={10}>
-      <h1>SMALL MODAL</h1>
+    <Modal isOpen onDismiss={console.debug} styleProps={{ mainBackgroundColor: 'cyan' }} maxHeight={'500px'}>
+      <h1 style={{ padding: '10px' }}>SMALL MODAL</h1>
     </Modal>
   )
 }
