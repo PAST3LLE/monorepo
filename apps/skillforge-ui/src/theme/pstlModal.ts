2b9a75b8906d909e9047cb1c35a832d9d79c87a8
w3stside <david.sato64@gmail.com>
2023-08-17 17:29:36 +0100

    skillforge-ui: update to latest

--- apps/skillforge-ui/src/theme/pstlModal.ts
+++ apps/skillforge-ui/src/theme/pstlModal.ts
@@ -8,25 +8,40 @@ export const FORGE_LOGO_URL_MAP = ikUrlToSimpleImageSrcSet(FORGE_LOGO_URL)
 export const pstlModalTheme = createTheme({
   DEFAULT: {
     modals: {
+      base: {
+        title: {
+          font: {
+            color: '#cbb9ee',
+            weight: 700,
+            letterSpacing: '-1.4px',
+            lineHeight: 0.82
+          }
+        }
+      },
       connection: {
-        backgroundImg: ASSETS_MAP.images.background.app,
+        background: {
+          backgroundImg: ASSETS_MAP.images.background.app
+        },
         baseFontSize: 20,
         helpers: { show: true },
         closeIcon: {
           size: '40px'
         },
-        title: { color: '#cbb9ee', fontWeight: 700, letterSpacing: '-1.4px', lineHeight: 0.82 },
         button: {
-          backgroundColor: '#301d4ea1',
-          connectedBackgroundColor: '#37b9927d',
+          background: {
+            background: '#301d4ea1',
+            connected: '#37b9927d'
+          },
+          font: {
+            color: 'ghostwhite',
+            size: '1em',
+            style: 'normal',
+            weight: 200,
+            letterSpacing: '-1px',
+            textShadow: '2px 2px 3px #0000005c',
+            textTransform: 'uppercase'
+          },
           border: { border: 'none', radius: '1em' },
-          color: 'ghostwhite',
-          fontSize: '1em',
-          fontStyle: 'normal',
-          fontWeight: 200,
-          letterSpacing: '-1px',
-          textShadow: '2px 2px 3px #0000005c',
-          textTransform: 'uppercase',
           hoverAnimations: true
         }
       }
