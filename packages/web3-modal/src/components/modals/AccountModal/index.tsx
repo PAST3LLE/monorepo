642d2bace6062630474aacc3045ca6eebabf72bd
w3stside <david.sato64@gmail.com>
2023-09-08 11:33:32 +0100

    web3-modal: remove basethem defaults

--- packages/web3-modal/src/components/modals/AccountModal/index.tsx
+++ packages/web3-modal/src/components/modals/AccountModal/index.tsx
@@ -8,7 +8,6 @@ import { ModalPropsCtrlState } from '../../../controllers/types/controllerTypes'
 import { useConnectDisconnect, usePstlWeb3Modal, useUserConnectionInfo } from '../../../hooks'
 import { useConnectedChainAndWalletLogo } from '../../../hooks/useLogos'
 import { PstlModalTheme } from '../../../theme'
-import BaseTheme from '../../../theme/baseTheme'
 import { ConnectorEnhanced } from '../../../types'
 import { BaseModalProps, ModalId } from '../common/types'
 import { WalletChainLogos } from './WalletChainLogos'
@@ -91,14 +90,8 @@ function AccountModalContent({ closeModalOnConnect, errorOptions }: PstlAccountM
       <AccountAddressBalanceRow
         borderRadius="1rem"
         padding="1em"
-        backgroundColor={
-          theme?.connection?.button?.background?.background ||
-          BaseTheme.modes.DEFAULT.modals.connection.button.background.background
-        }
-        border={
-          theme?.account?.container?.walletAndNetwork?.border?.border ||
-          BaseTheme.modes.DEFAULT.modals.account.container.walletAndNetwork.border.border
-        }
+        backgroundColor={theme?.connection?.button?.background?.background}
+        border={theme?.account?.container?.walletAndNetwork?.border?.border}
       >
         <Column>
           <Row
@@ -148,20 +141,12 @@ function AccountModalContent({ closeModalOnConnect, errorOptions }: PstlAccountM
       <AccountWalletNetworkRow
         width="100%"
         padding="1em"
-        border={
-          theme?.account?.container?.addressAndBalance?.border?.border ||
-          BaseTheme.modes.DEFAULT.modals.account.container.addressAndBalance.border.border
-        }
-        borderRadius={
-          theme?.account?.container?.addressAndBalance?.border?.radius ||
-          BaseTheme.modes.DEFAULT.modals.account.container.addressAndBalance.border.radius
-        }
+        border={theme?.account?.container?.addressAndBalance?.border?.border}
+        borderRadius={theme?.account?.container?.addressAndBalance?.border?.radius}
         backgroundColor={
           isUnsupportedChain
-            ? theme?.account?.container?.addressAndBalance?.background?.unsupported ||
-              BaseTheme.modes.DEFAULT.modals.account.container.addressAndBalance.background.unsupported
-            : theme?.connection?.button?.background?.background ||
-              BaseTheme.modes.DEFAULT.modals.connection.button.background.background
+            ? theme?.account?.container?.addressAndBalance?.background?.unsupported
+            : theme?.connection?.button?.background?.background
         }
       >
         <Column width="100%" maxWidth={400}>
