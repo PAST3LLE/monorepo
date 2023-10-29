9e6efed1f9009be26dbe984ef96cfcab68efa254
w3stside <david.sato64@gmail.com>
2023-10-29 01:14:03 +0100

    wagmi-connectors: fix gitignore and add back in w3aconenctor

---
+++ packages/wagmi-connectors/src/PstlWeb3AuthConnector/index.ts
@@ -0,0 +1,172 @@
+// Web3Auth Libraries
+import { CHAIN_NAMESPACES, WALLET_ADAPTERS } from '@web3auth/base'
+import { IPlugin } from '@web3auth/base-plugin'
+import { Web3Auth, Web3AuthOptions } from '@web3auth/modal'
+import { OpenloginAdapter, OpenloginLoginParams } from '@web3auth/openlogin-adapter'
+import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
+import { Chain } from 'viem'
+
+export interface PstlWeb3AuthConnectorProps {
+  themeInfo?: {
+    mode?: 'light' | 'dark'
+    primary?: string
+  }
+  zIndex?: number
+  network: Web3AuthOptions['web3AuthNetwork']
+  storageKey?: Web3AuthOptions['storageKey']
+  preset?: 'DISALLOW_EXTERNAL_WALLETS' | 'ALLOW_EXTERNAL_WALLETS'
+  projectId: string
+  appName: string
+  url?: string
+  appLogoLight?: string
+  appLogoDark?: string
+  listingName?: string
+  listingLogo?: string
+  listingDetails?: string
+  loginMethodsOrder?: string[]
+  mfaLevel?: OpenloginLoginParams['mfaLevel']
+  uxMode?: 'popup' | 'redirect'
+  configureAdditionalConnectors?: () => IPlugin[] | undefined
+}
+
+/**
+     * @description Web3Auth modal connector
+     * @param options - options / config object:
+     * @example 
+     interface Options {
+        themeInfo?: {
+          mode?: 'light' | 'dark'
+          primary?: string
+        }
+        chains: ChainsPartialReadonly<ID>
+        zIndex?: number
+        network: Web3AuthOptions['web3AuthNetwork']
+        storageKey?: Web3AuthOptions['storageKey']
+        preset?: 'DISALLOW_EXTERNAL_WALLETS' | 'ALLOW_EXTERNAL_WALLETS'
+        projectId: string
+        appName: string
+        url?: string
+        appLogoLight?: string
+        appLogoDark?: string
+        listingName?: string
+        listingLogo?: string
+        listingDetails?: string
+        loginMethodsOrder?: string[]
+        mfaLevel?: OpenloginLoginParams['mfaLevel']
+        uxMode?: 'popup' | 'redirect'
+        configureAdditionalConnectors?: () => IPlugin[] | undefined
+     }
+    */
+export function PstlWeb3AuthConnector(chains: Chain[], options: PstlWeb3AuthConnectorProps): Web3AuthConnector {
+  const {
+    network = 'testnet',
+    appName,
+    projectId,
+    themeInfo = {
+      mode: 'dark'
+    },
+    storageKey,
+    appLogoDark,
+    appLogoLight,
+    url,
+    loginMethodsOrder,
+    mfaLevel,
+    uxMode,
+    zIndex = 1000,
+    preset = 'ALLOW_EXTERNAL_WALLETS',
+    configureAdditionalConnectors
+  } = options
+  if (!projectId) throw new Error('Missing REACT_APP_WEB3AUTH_ID! Check env.')
+
+  const chainConfig = {
+    chainNamespace: CHAIN_NAMESPACES.EIP155,
+    chainId: '0x' + (chains[0].id as number).toString(16),
+    rpcTarget: chains[0].rpcUrls.default.http[0], // This is the public RPC we have added, please pass on your own endpoint while creating an app
+    displayName: chains[0].name,
+    tickerName: chains[0].nativeCurrency?.name,
+    ticker: chains[0].nativeCurrency?.symbol
+  }
+
+  // Create Web3Auth Instance
+  const web3AuthInstance = new Web3Auth({
+    clientId: projectId,
+    chainConfig,
+    web3AuthNetwork: network,
+    authMode: 'DAPP',
+    uiConfig: {
+      appName,
+      theme: themeInfo.mode,
+      loginMethodsOrder,
+      defaultLanguage: 'en',
+      appLogo: themeInfo.mode === 'light' ? appLogoLight : appLogoDark,
+      modalZIndex: zIndex.toString()
+    }
+  })
+  // Add openlogin adapter for customisations
+  const openloginAdapterInstance = new OpenloginAdapter({
+    adapterSettings: {
+      network,
+      uxMode,
+      storageKey,
+      whiteLabel: {
+        name: appName,
+        url,
+        logoLight: appLogoLight,
+        logoDark: appLogoDark,
+        defaultLanguage: 'en',
+        theme: {
+          primary: themeInfo.primary
+        },
+        dark: themeInfo.mode === 'dark' // whether to enable dark mode. defaultValue: false
+      }
+    },
+    loginSettings: {
+      mfaLevel
+    }
+  })
+  web3AuthInstance.configureAdapter(openloginAdapterInstance)
+
+  // custom connector callback
+  const pluginsList = configureAdditionalConnectors?.()
+  if (!!pluginsList?.length) {
+    pluginsList?.forEach((plugin) => {
+      web3AuthInstance.addPlugin(plugin)
+    })
+  }
+
+  return new Web3AuthConnector({
+    chains,
+    options: {
+      web3AuthInstance,
+      modalConfig:
+        preset === 'DISALLOW_EXTERNAL_WALLETS'
+          ? {
+              [WALLET_ADAPTERS.METAMASK]: {
+                label: 'MetaMask',
+                showOnDesktop: false,
+                showOnModal: false,
+                showOnMobile: false
+              },
+              [WALLET_ADAPTERS.TORUS_EVM]: {
+                label: 'Torus',
+                showOnDesktop: false,
+                showOnModal: false,
+                showOnMobile: false
+              },
+              [WALLET_ADAPTERS.WALLET_CONNECT_V1]: {
+                label: 'WalletConnect [v1]',
+                showOnDesktop: false,
+                showOnModal: false,
+                showOnMobile: false
+              },
+              [WALLET_ADAPTERS.WALLET_CONNECT_V2]: {
+                label: 'WalletConnect [v2]',
+                showOnDesktop: false,
+                showOnModal: false,
+                showOnMobile: false
+              }
+            }
+          : undefined
+    }
+  })
+}
