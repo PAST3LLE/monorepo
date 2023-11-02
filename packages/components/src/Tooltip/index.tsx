78e6a2cac4b2c1c4cc1884cef1e1b308283f7720
w3stside <david.sato64@gmail.com>
2023-11-02 13:47:48 +0000

    components: fix theme + macro issues

--- packages/components/src/Tooltip/index.tsx
+++ packages/components/src/Tooltip/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react'
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 
 import { Popover, PopoverProps } from '../Popover'
 
