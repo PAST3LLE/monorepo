81a592ceab756f6c863f4b116f36cc8a2d21b784
w3stside <david.sato64@gmail.com>
2023-11-24 00:11:05 +0100

    web3-modal: PoweredByLabel

---
+++ packages/web3-modal/src/components/modals/common/PoweredByLabel.tsx
@@ -0,0 +1,52 @@
+import { Row } from '@past3lle/components'
+import { setBestTextColour } from '@past3lle/theme'
+import { MakeOptional } from '@past3lle/types'
+import React from 'react'
+import styled from 'styled-components'
+
+import { ContainerThemeTypes, ModalText } from './styled'
+
+const PoweredByLabelContainer = styled(Row).attrs({
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginTop: 'auto'
+})<ContainerThemeTypes>`
+  z-index: 500;
+  > ${ModalText} {
+    color: ${({ theme: { modals }, modal, node }) =>
+      !modals?.[modal]?.background?.url && !!modals?.[modal]?.container?.[node]?.background
+        ? setBestTextColour(modals?.[modal]?.container?.[node]?.background as string)
+        : modals?.[modal]?.button?.[node]?.font?.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+  }
+
+  a {
+    color: inherit;
+    outline: none;
+    text-decoration: none;
+
+    > strong {
+      font-weight: 400;
+      font-variation-settings: 'wght' 400;
+    }
+  }
+`
+
+export function PoweredByLabel({ modal = 'base', node = 'main' }: MakeOptional<ContainerThemeTypes, 'modal' | 'node'>) {
+  return (
+    <PoweredByLabelContainer modal={modal} node={node} title="POWERED BY PAST3LLE LABS">
+      <ModalText modal="base" node="subHeader" fontSize={10}>
+        <img
+          src="https://avatars.githubusercontent.com/u/121028394?s=400&u=bd6f1cf385e9af8466418cea88fe9d11c9654a9f&v=4"
+          width="20px"
+        />
+        <a href="https://pastellelabs.com?=referrer=pstl-web3-modal" target="_blank" referrerPolicy="no-referrer">
+          POWERED BY <strong>PAST3LLE LABS</strong>
+        </a>
+      </ModalText>
+    </PoweredByLabelContainer>
+  )
+}
