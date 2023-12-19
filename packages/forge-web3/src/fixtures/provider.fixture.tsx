fdb096e1806ff4217bd26164ddac2a80d3530078
w3stside <david.sato64@gmail.com>
2023-12-19 17:27:30 +0000

    forge-web3: revert changes

--- packages/forge-web3/src/fixtures/provider.fixture.tsx
+++ packages/forge-web3/src/fixtures/provider.fixture.tsx
@@ -1,5 +1,9 @@
+import { RowCenter, SpinnerCircle } from '@past3lle/components'
+import { useAddPendingTransaction } from '@past3lle/web3-modal'
 import { Web3Button } from '@web3modal/react'
-import React from 'react'
+import React, { Fragment, useCallback, useState } from 'react'
+import { parseEther } from 'viem'
+import { Address, useBalance, useSendTransaction } from 'wagmi'
 
 import { ForgeW3Providers, useForgeUnpreparedClaimLockedSkill, useW3Modal, useW3UserConnectionInfo } from '..'
 import { THEME, commonProps, contractProps } from './config'
@@ -15,24 +19,75 @@ import { THEME, commonProps, contractProps } from './config'
 
 function InnerApp() {
   const { open } = useW3Modal()
-  const { address, balance: balanceInfo } = useW3UserConnectionInfo()
+  const { address } = useW3UserConnectionInfo()
 
-  const data = useForgeUnpreparedClaimLockedSkill({
-    token: '0x1b18aC6D5371FeD52521964145E2c8aAF7571a88',
-    id: BigInt(3000)
-  })
+  // const lockedSkillData = useForgeUnpreparedClaimLockedSkill({
+  //   token: '0x1b18aC6D5371FeD52521964145E2c8aAF7571a88',
+  //   id: BigInt(3000)
+  // })
+  const { data, refetch } = useBalance({ address })
+  const [sendEthVal, setSendEthVal] = useState('0')
+  const [addressToSendTo, setAddress] = useState('')
+
+  const sendApi = useSendTransaction()
+
+  const addPendingTransaction = useAddPendingTransaction()
+
+  const [currentTx, setTx] = useState('')
+  const handleSendTransaction = useCallback(
+    async (args: { value: bigint; to: string }) => {
+      setTx('pending')
+      sendApi
+        .sendTransactionAsync(args)
+        .then((tx) => {
+          setTx(tx.hash)
+          addPendingTransaction(tx.hash)
+        })
+        .catch((error) => {
+          throw error
+        })
+        .finally(() => {
+          setTx('')
+        })
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [sendApi]
+  )
 
   return (
-    <div>
-      <button onClick={() => open({ route: address ? 'Account' : 'ConnectWallet' }) as any}>
-        Open Pstl Web3 Modal
-      </button>
-      <h1>{address || 'Disconnected'}</h1>
-      <h1>{(balanceInfo.data?.formatted || 0) + ' ETH' || '0 ETH'}</h1>
-      <button disabled={!data.writeAsync} onClick={() => data.writeAsync?.()}>
-        Claim skill
-      </button>
-    </div>
+    <>
+      {/* @ts-ignore */}
+      <button onClick={open}>Open W3 Modal</button>
+      <h1>Here has wagmi access</h1>
+      <p>Address: {address}</p>
+      <button onClick={() => refetch()}>Get balance</button>
+      <p>Balance: {data?.formatted}</p>
+      <br />
+
+      <p>Send ETH</p>
+      <>
+        {!!currentTx && status !== 'success' && (
+          <RowCenter width="300px" gap="1rem">
+            <h4>TX {currentTx} in progress...</h4>
+            <SpinnerCircle filter="invert(1)" size={100} />
+          </RowCenter>
+        )}
+      </>
+
+      <>
+        <p>
+          Amount
+          <input type="text" value={sendEthVal} onChange={(e: any) => setSendEthVal(e.target.value)} />
+        </p>
+        <p>
+          To
+          <input type="text" value={addressToSendTo} onChange={(e: any) => setAddress(e.target.value)} />
+        </p>
+        <button onClick={() => handleSendTransaction({ value: parseEther(sendEthVal), to: addressToSendTo })}>
+          Send to {addressToSendTo || 'N/A'}
+        </button>
+      </>
+    </>
   )
 }
 
@@ -50,9 +105,9 @@ function App() {
         name: commonProps.appName,
         web3: {
           connectors: {
-            overrides: { 
+            overrides: {
               walletconnect: {
-                async customConnect({store}) {
+                async customConnect({ store }) {
                   return store.walletConnect.open()
                 }
               }
