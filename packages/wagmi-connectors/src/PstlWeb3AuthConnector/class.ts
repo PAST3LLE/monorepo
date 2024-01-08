0a22519d4f29cb7add13e6f1b7352d835a17ba5c
w3stside <david.sato64@gmail.com>
2024-01-08 13:38:50 +0000

    wagmi-connectors: PstlWeb3AuthConnector -- comments on change

--- packages/wagmi-connectors/src/PstlWeb3AuthConnector/class.ts
+++ packages/wagmi-connectors/src/PstlWeb3AuthConnector/class.ts
@@ -1,24 +1,31 @@
 import { BaseAdapter } from '@web3auth/base'
+import { IPlugin } from '@web3auth/base-plugin'
 import { Web3AuthConnector } from '@web3auth/web3auth-wagmi-connector'
 import { ConnectorData } from 'wagmi'
 
-type AuxWeb3AuthConnectorParams<BA extends BaseAdapter<any>> = ConstructorParameters<typeof Web3AuthConnector>[0] & {
-  options: ConstructorParameters<typeof Web3AuthConnector>[0]['options'] & { adapter?: BA }
+type AuxWeb3AuthConnectorParams<BA extends BaseAdapter<any>, PL extends IPlugin[]> = ConstructorParameters<
+  typeof Web3AuthConnector
+>[0] & {
+  options: ConstructorParameters<typeof Web3AuthConnector>[0]['options'] & { adapter?: BA; plugins?: PL }
 }
 
-export class AuxWeb3AuthConnector<BA extends BaseAdapter<any>> extends Web3AuthConnector {
+export class AuxWeb3AuthConnector<BA extends BaseAdapter<any>, PL extends IPlugin[]> extends Web3AuthConnector {
   protected adapter: BA | undefined
+  protected plugins: PL | undefined
 
-  constructor(params: AuxWeb3AuthConnectorParams<BA>) {
+  constructor(params: AuxWeb3AuthConnectorParams<BA, PL>) {
     super(params)
     this.adapter = params?.options?.adapter
+    this.plugins = params?.options?.plugins
   }
 
   // Enhance super connect and listen to changes on adapter
   async connect({ chainId }: { chainId?: number } = {}): Promise<Required<ConnectorData>> {
     try {
       const data = await super.connect({ chainId })
-      // Listen to emitted events
+      // Listen to emitted events FROM ADAPTER (current web3auth version ~7.x doesnt listen to this)
+      // Which never prompts wagmi to update the connector
+      // See issue: https://github.com/Web3Auth/web3auth-wagmi-connector/pull/113
       this.adapter?.provider?.on('accountsChanged', this.onAccountsChanged)
       this.adapter?.provider?.on('chainChanged', this.onChainChanged)
 
