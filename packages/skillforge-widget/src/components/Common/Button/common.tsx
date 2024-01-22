864e5e7dc851b2bf3686f5d0e891ad9e5079dac7
daveo <david.sato64@gmail.com>
2024-01-22 17:53:17 +0000

    skillforge-widget: css fixes (#44)

--- packages/skillforge-widget/src/components/Common/Button/common.tsx
+++ packages/skillforge-widget/src/components/Common/Button/common.tsx
@@ -45,14 +45,16 @@ export const ThemedButton = styled(Button).attrs(() => ({}))<{
   transition: box-shadow 0.1s ease-in-out, transform 0.1s ease-in-out;
 `
 
-export const ThemedButtonExternalLink = styled(ExternalLink)<{
+export const ThemedButtonExternalLink = styled(ExternalLink).attrs({
+  minWidth: 200
+})<{
   disabled?: boolean
   fontSize?: string
   fullWidth?: boolean
 }>`
   z-index: 1;
   padding: 1rem 2rem;
-  flex: 1 1 200px;
+  flex: 1;
   text-align: center;
   ${({ fontSize }) => fontSize && `font-size: ${fontSize};`}
   background-color: ${({ theme, disabled }) => (disabled ? theme.rarity.common.backgroundColor : theme.mainBg)};
@@ -68,9 +70,11 @@ export const ThemedButtonExternalLink = styled(ExternalLink)<{
     `}
 `
 
-export const ThemedButtonActions = styled(ThemedButton)`
+export const ThemedButtonActions = styled(ThemedButton).attrs({
+  minWidth: 200
+})`
   padding: 1rem 2rem;
-  flex: 1 1 200px;
+  flex: 1;
   z-index: 1;
   text-align: center;
   box-shadow: none;
