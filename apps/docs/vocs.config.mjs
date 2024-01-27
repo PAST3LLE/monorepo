92002c8d29bed24d1b8e50f6b6ff89909dd330ad
w3stside <david.sato64@gmail.com>
2024-01-27 11:07:59 +0000

    chore(docs): add config files

---
+++ apps/docs/vocs.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  description:
+    'PAST3LLE monorepo package suite offers Web3 and Web2 hooks/components/utils/assets/widgets for launching web3 (d)Apps.',
+  title: 'PAST3LLE MONOREPO',
+  rootDir: './src'
+})
