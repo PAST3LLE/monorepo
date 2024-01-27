7d2aac0e1ce6af1f6d77dd117642ae8d7aa53ba0
w3stside <david.sato64@gmail.com>
2024-01-27 13:17:42 +0000

    docs(root): add initial files

---
+++ apps/docs/src/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <div>Released under the MIT License.</div>
+      <div>Copyright © 2022-present PAST3LLE LABS, LDA.</div>
+    </div>
+  )
+}
