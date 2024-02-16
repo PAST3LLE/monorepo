ae55582cbda97421609f72d6db315ba753b270f4
w3stside <david.sato64@gmail.com>
2024-02-16 11:56:40 +0000

    docs: update

--- apps/docs/vocs.config.mjs
+++ apps/docs/vocs.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vocs'
 export default defineConfig({
   ogImageUrl: 'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description', 
   description:
-    'PAST3LLE monorepo package suite offers Web3 and Web2 hooks/components/utils/assets/widgets for launching web3 (d)Apps.',
-  title: 'PAST3LLE MONOREPO',
+    'PAST3LLE Labs package suite offers Web3 and Web2 hooks/components/utils/assets/widgets for launching web3 (d)Apps.',
+  title: 'PAST3LLE LABS',
   rootDir: './src'
 })
